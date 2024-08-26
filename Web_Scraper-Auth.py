f62f87615b48e864d1d4bd95065f50eab2112397
th596u <tylerdhardy2@gmail.com>
2024-08-26 11:55:43 -0400

    Web Scraper Start

--- Web_Scraper-Auth.py
+++ Web_Scraper-Auth.py
@@ -1,4 +1,23 @@
-import requests
-res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
-type(res)
-print(res.text[:250])
+#
+# import requests
+import getpass
+# from bs4 import BeautifulSoup
+
+mypass = getpass.getpass("Enter your password: ")
+
+header = {
+	'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'
+}
+
+login_data = {
+	'username': 'th596u',
+	'password': 'mypass'
+}
+
+# url = 'https://oidc.idp.elogin.att.com/mga/sps/authsvc?PolicyId=urn:ibm:security:authentication:asf:pass'
+# s = requests.Session()
+# data = s.get(url, headers = header)
+
+# print(data)
+print(login_data)
+# s.close()
